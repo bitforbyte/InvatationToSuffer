f4fe8f4fc7675b6259871e3148ae60b4bf66eb89
Kendall Nicley <ken.nicley@outlook.com>
2019-04-08 01:00:31 -0400

    Added Comments

--- kpart1.py
+++ kpart1.py
@@ -3,10 +3,11 @@ import string
 import sys
 import ast
 
+# Batch to hold the values from each batch
 class Batch:
 	def __init__(self, senders, recievers):
-		self.senders = senders
-		self.recievers = recievers
+		self.senders = senders						# Those who sent messages
+		self.recievers = recievers					# Those who recieved messages
 
 class User:
 	def __init__(self, name):
@@ -26,21 +27,19 @@ class User:
 
 if __name__== "__main__":
 
-	numUsers = 260 # N recipients
-	numBatch = 32  # b for batch size
+	numUsers = 260		# N recipients
+	numBatch = 32		# b for batch size
 
-	# Number of users Contributed to each round
-	# numCont = t
 	users = []
 	userDict = {}
 	userDictV = {}
 	for i in range(0, 26):
-		letter = dict(zip(range(0, 26), string.ascii_lowercase))
+		letter = dict(zip(range(0, 26), string.ascii_lowercase))	
 		for j in range(0, 10):
-			letterName = letter[i] + string.digits[j]
-			users.append(User(letterName))
-			userDictV[letterName] = 1/2			# V is probability to send to each recipient
-			userDict[letterName] = 0
+			letterName = letter[i] + string.digits[j]	# Get the letter names (a-z)(0-9)
+			users.append(User(letterName))				# Users list that will hold each of the users in a class
+			userDictV[letterName] = 1/2					# V is probability to send to each recipient (1/2 for two friends)
+			userDict[letterName] = 0					# assign the dictionary for the U and V default values
 	
 	# Copy the dictionary into each of the dictionaries
 	for user in users:
@@ -53,15 +52,18 @@ if __name__== "__main__":
 	# Read in the batches into a list
 	# List to hold each batch
 	batches = []
-	lines = sys.stdin.readlines()
+	lines = sys.stdin.readlines()	# Holds the raw input text
 
-	numBatch = 0
+	numBatch = 0	# number of batches used for index (used for debug printing)
+	
+	# Loop through lines by 2 for S and R pairs
 	for i in range(0, len(lines)-1,2):
-		# Trim the sender and reciever lists
-		sendersList = ast.literal_eval(lines[i][2:]) 
+
+		# Trim the sender and reciever lists to obtain a list of names
+		sendersList = ast.literal_eval(lines[i][2:])		
 		recieversList = ast.literal_eval(lines[i+1][2:])
 
-		# assign the Senders and Recievers into the rounds list
+		# assign the Senders and Recievers into the batch list
 		batches.append(Batch(sendersList, recieversList))
 
 		#print(rounds[numBatch].senders)
@@ -74,20 +76,29 @@ if __name__== "__main__":
 	# Calculate the messages send and not sent by each user
 	# Not sent
 	for user in users:
+		# Traverse each batch to determine values for users
 		for batch in batches:
-			if user.name not in batch.senders:				# User hasn't sent a message
-				# used to increment number of batches not send t'
+
+			# If the user hasn't sent a message
+			if user.name not in batch.senders:
+
+				# Increment t' (number of batches not spoken in)
 				user.tPrime += 1
+
+				# Scan the recievers and set value to probability of message send (1/(Batch Size))
 				for receivers in batch.recievers:
 					user.vecU[receivers] = 1/numBatch	# numBatch(b) = 32
 
-			else:	# User has sent a message
-				# Increment number of messages sent t
+			else:	
+				# User has sent a message in this batch
+				# Increment t (number of batches spoken in)
 				user.t += 1
+
+				# TODO not sure if this is correct
 				for receivers in batch.recievers:
 					user.vecO[receivers] = 1/numBatch	# numBatch(b) = 32
 
-
+		# Batches have been scanned
 		# Calculate the big O and U using the Arithmetic mean
 		if user.tPrime != 0:
 			user.bigU = (1/user.tPrime) * (sum(user.vecU.values()))
@@ -99,6 +110,7 @@ if __name__== "__main__":
 		#print(user.bigU)
 		#print(user.bigO)
 		
+		# Assign the vector V (Final Formula)
 		for val in user.vecV:
 			user.vecV[val] = user.vecV[val] * ((32 * user.bigO) - (32 - 1)* user.bigU)
 			print(user.vecV[val])

58f3c94e826ee28fd9386e26be24c390ae83c65e
bitforbyte <ken.nicley@outlook.com>
2019-04-10 03:37:10 -0400

    Rewrote kpart1

--- kpart1.py
+++ kpart1.py
@@ -9,57 +9,89 @@ class Batch:
 	def __init__(self, senders, recievers):
 		self.senders = senders						# Those who sent messages
 		self.recievers = recievers					# Those who recieved messages
-
-class User:
-	def __init__(self, name):
-		self.name = name
-		self.vecV = []		# Vector V: Sum of n=0 -> Infinity (n * Pm(n)) where n is number of messages sent by user. Pm is a probability function
-		self.vecU = []		# Vector U: Background distribution: t' of batches user didn't send message( each batch i, construct vector ui, elements are 1/b for reveivers else 0)
-		self.vecO =	[]		# Vector o: Rounds Alice sent message, the number of messages mi, sent
-		self.tPrime = 0		# t'
-		self.t = 0			# t
-		self.bigO = 0		# Arithmetic mean of large set vecO
-		self.bigU = 0		# Arithmetic mean of large set vecU
-
-	# Process each of the batches to construct each of the vectors
-	def Process(self, batches):
-		pass
 	
+class User:
+	def __init__(self, username):
+		self.name = username
+		self.numUsers = 260				# N recipients
+		self.numBatch = 32				# b for batch size
+
+	def setInfo(self, users, batches):
+		self.batches = batches
+		self.users = users
+
+	def findFriends(self):
+		# Find u
+		tPrime = 0
+		bigU = {}
+		for i in self.users:
+			bigU[i.name] = 0
+
+		# Find the batch where user doesn't speak
+		for batch in self.batches:
+			# If the user isn't a sender
+			if self.name not in batch.senders:
+				tPrime += 1
+
+				# For each user who recieved add 1/b to their background distribution to get SUM(ui) from 1 - t'
+				for received in batch.recievers:
+					if self.name != received:
+						if bigU[received] == 0:
+							bigU[received] = 1/self.numBatch
+						else:
+							bigU[received] += 1/self.numBatch
+		
+		# Multiply 1/t' to each of the users resulting in Ubar
+		for users in bigU:				
+			bigU[users] *= (1/tPrime)
+
+		t = 0
+		bigO = {}
+		for i in self.users:
+			bigO[i.name] = 0
+
+		# Loop through the batches
+		for batch in self.batches:
+			# See if the name is in the senders
+			if self.name in batch.senders:
+				t += 1
+
+				for received in batch.recievers:
+					if self.name != received:
+						if bigO[received] == 0:
+							bigO[received] = 1/self.numBatch
+						else:
+							bigO[received] += 1/self.numBatch
+
+		for users in bigO:
+			bigO[users] += (1/t)
+		
+
+		vecV = {}
+		for i in self.users:
+			vecV[i.name] = (1/1) * ((self.numBatch * bigO[i.name]) - (self.numBatch - 1) * bigU[i.name])
+
+		return vecV
+		# Return list of most likely friends
 
 if __name__== "__main__":
 
-	numUsers = 260		# N recipients
-	numBatch = 32		# b for batch size
-
 	users = []
-	userDict = {}
-	userDictV = {}
 	for i in range(0, 26):
 		letter = dict(zip(range(0, 26), string.ascii_lowercase))	
 		for j in range(0, 10):
 			letterName = letter[i] + string.digits[j]	# Get the letter names (a-z)(0-9)
-			users.append(User(letterName))				# Users list that will hold each of the users in a class
-			userDictV[letterName] = 1/2					# V is probability to send to each recipient (1/2 for two friends)
-			userDict[letterName] = 0					# assign the dictionary for the U and V default values
-	
-	# Copy the dictionary into each of the dictionaries
-	for user in users:
-		user.vecV = userDictV.copy()
-		user.vecU = userDict.copy()
-		user.vecO = userDict.copy()
-		
-		
+			users.append(User(letterName))				# Users list that will hold each of the users in a class		
 
 	# Read in the batches into a list
 	# List to hold each batch
 	batches = []
 
-
 	#lines = sys.stdin.readlines()	# Holds the raw input text
 	file = open("dataset.raw", "r")
 	lines = file.readlines()
 	file.close()
-	numBatch = 0	# number of batches used for index (used for debug printing)
+	#numBatch = 0	# number of batches used for index (used for debug printing)
 	
 	# Loop through lines by 2 for S and R pairs
 	for i in range(0, len(lines)-1,2):
@@ -71,81 +103,13 @@ if __name__== "__main__":
 		# assign the Senders and Recievers into the batch list
 		batches.append(Batch(sendersList, recieversList))
 
-		#print(rounds[numBatch].senders)
-		#print(rounds[numBatch].recievers)
 
-		# Increment index by one to use correct placement
-		numBatch+=1
-		
-	# At this point we have all rounds with who spoke in each round
-	# Calculate the messages send and not sent by each user
-	# Not sent
-	test = 0
-	for user in users:
-		# Traverse each batch to determine values for users
-		for batch in batches:
+	users[0].setInfo(users, batches)
+	final = users[0].findFriends()
 
-			# If the user hasn't sent a message
-			if user.name not in batch.senders:
-				# Construct the background distribution u vector
-
-				# Increment t' (number of batches not spoken in)
-				user.tPrime += 1
-				
-
-				# Scan the recievers and set value to probability of message send (1/(Batch Size))
-				for receiver in batch.recievers:
-					if user.vecU[receiver] == 0:
-						user.vecU[receiver] = 1/numBatch	# numBatch(b) = 32
-					else:
-						user.vecU[receiver] += 1/numBatch	# numBatch(b) = 32
-
-			else:	
-				# User has sent a message in this batch
-				# Increment t (number of batches spoken in)
-				user.t += 1
-
-				# TODO not sure if this is correct
-				for receivers in batch.recievers:
-					if user.vecO[receivers] == 0:
-						user.vecO[receivers] = 1/numBatch	# numBatch(b) = 32
-					else:
-						user.vecO[receivers] += 1/numBatch 
-				
-
-
-		# Batches have been scanned
-		# Calculate the big O and U using the Arithmetic mean
-		if user.tPrime != 0:
-			#print(user.name)
-			for key, val in user.vecU.items():
-				#print("%s: %f" % (key, val))
-				user.vecU[key] = val * (1/(user.tPrime))
-
-
-		if user.t != 0:
-			for key, val in user.vecO.items():
-				user.vecO[key] = val * (1/(user.tPrime))
-
-		#print(user.name)
-		#print(user.bigU)
-		#print(user.bigO)
-		
-		
-	# Assign the vector V (Final Formula)
-	print("USER:: " + users[0].name)
-	for val in users[0].vecV:
-		print(val + ' ', end='')
-
-		user.vecV[val] = ((32 * user.vecO[val]) - (32 - 1) * user.vecU[val])
-		print(user.vecV[val])
-		#print(val + ' ', end='')
-		#print(user.vecV[val])
-
-
-	print()
-	#sortedUsers = [ (v,k) for k,v in users[0].vecV.items() ]
-	#sortedUsers.sort(reverse=True)
-
-	#for v,k in sortedUsers:
-	#	print ("%s: %f" % (k, v))
+	sorted_final = sorted(final.items(), key=lambda kv: kv[1])
+	print (sorted_final)
+	#for user in users:
+	#	user.setInfo(users, batches)
+	#	user.findFriends()
+		#print(user.findFriends())

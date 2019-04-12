0d936d18ffd7f2e53ca6cc069116b10fbeb944b4
bitforbyte <knicley2@hydra1.eecs.utk.edu>
2019-04-12 00:50:33 -0400

    Single PEER command working

--- kToynetHarvester.py
+++ kToynetHarvester.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import socket
 
 TCP_IP = '160.36.57.98'
-TCP_PORT = 10722
+TCP_PORT = 10732
 BUFFER_SIZE = 1024
-MESSAGE = "PEERS"
+MESSAGE = "PEERS\n".encode('utf-8')
+
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((TCP_IP, TCP_PORT))
 soc.send(MESSAGE)
+
 data = soc.recv(BUFFER_SIZE)
+print('received:' + repr(data))
 soc.close() 

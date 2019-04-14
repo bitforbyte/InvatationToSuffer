f9583707da584de2fd50c839ab6664768e90f568
bitforbyte <ken.nicley@outlook.com>
2019-04-13 23:48:58 -0400

    Added Network class and cleaned up code

--- kToynetHarvester.py
+++ kToynetHarvester.py
@@ -2,134 +2,131 @@
 import time
 import socket
 import re
+import operator
 
 TCP_IP = '160.36.57.98'
 BUFFER_SIZE = 1024
+
+class Network:
+    def __init__(self):
+        self.nodes = []
+    
+    # Add user to the network
+    def addUser(self, node):
+        exists = 0
+        for i in self.nodes:
+            if i.id == node.id:
+                exists = 1
+                break
+        # If it isn't in the nodes add it
+        if exists == 0:
+            self.nodes.append(node)
+
+    # Write the users to the given file
+    def writeNetwork(self, fil):
+        fileName = open(fil, 'w')
+        self.nodes.sort(key=lambda x:str(x.port))
+        for node in self.nodes:
+            fileName.write(node.id + "@" + TCP_IP + ":" + str(node.port) + "\n")
+        fileName.close()
+
+    def findPeers(self, nodeID, soc):
+        counter = 0
+
+        # Send PEERS commands until 
+        while(counter < 100):
+            
+            soc.send("PEERS\n".encode('utf-8'))
+            data = soc.recv(BUFFER_SIZE)
+            
+            parsedata = data.decode('utf-8')
+
+            # Check to see if Punished
+            if parsedata.find('BANNED') != -1:
+                num = re.search("\d+\.\d+", parsedata)
+                print("BANNED: Flail thyself for %s minutes" % num.group(0))
+                raise ConnectionResetError()
+
+            dataBreak = parsedata.split('\n')
+
+            # Loop through the split data
+            for each in dataBreak:
+
+                # If it's a string
+                if each:
+                    # Attempt to read the PEER data
+                    try:
+                        # Regex commands to find info
+                        nIDSearch = re.search('8(.*)@', each)
+                        #ipAddrSearch = re.search('@(.*):', each)
+                        nodePortSearch = re.search(':(.*)', each)
+                        
+                        # Make sure they searches exists
+                        if nIDSearch and nodePortSearch:
+                            # re returns structure group(0) is the value found
+                            nID = nIDSearch.group(0)[:-1]
+                            nodePort = nodePortSearch.group(0)[1:]
+
+                            # Make sure the id is 64 characters long
+                            if len(nID) == 64:
+                                # Test if the peer exists
+                                test = 0
+                                for i in self.nodes:
+                                    if i.id == nID:
+                                        test = 1
+                                        break
+
+                                if test == 1: 
+                                    counter += 1
+                                else:
+                                    # Add the nodes
+                                    self.nodes.append(Node(nID, nodePort))
+
+                                    # Reset the counter if one is found
+                                    counter = 0 
+                    except AttributeError:
+                        print("Error:")       
+        return self.nodes
 
 class Node:
     def __init__(self, nodeId, port):
         self.id = nodeId
         self.port = port
-    
-class bot:
-    def __init__(self, port):
-        self.host = TCP_IP
-        self.port = port
-        self.soc = None
-    
-
-def findPeers(nodeID, soc):
-    nodes = []
-    counter = 0
-
-    # Send PEERS commands until 
-    while(counter < 100):
-        
-        soc.send("PEERS\n".encode('utf-8'))
-        data = soc.recv(BUFFER_SIZE)
-        
-        parsedata = data.decode('utf-8')
-
-        # Check to see if Punished
-        if parsedata.find('BANNED') != -1:
-            num = re.search("\d+\.\d+", parsedata)
-            print("BANNED: Flail thyself for %s minutes" % num.group(0))
-            raise ConnectionResetError()
-
-        dataBreak = parsedata.split('\n')
-
-        # Loop through the split data
-        for each in dataBreak:
-
-            # If it's a string
-            if each:
-                # Attempt to read the PEER data
-                try:
-                    # Regex commands to find info
-                    nIDSearch = re.search('8(.*)@', each)
-                    #ipAddrSearch = re.search('@(.*):', each)
-                    nodePortSearch = re.search(':(.*)', each)
-                    
-                    if nIDSearch and nodePortSearch:
-                        # re returns structure group(0) is the value found
-                        nID = nIDSearch.group(0)[:-1]
-                        nodePort = nodePortSearch.group(0)[1:]
-
-                        if len(nID) == 64:
-                            # Test if the peer exists
-                            test = 0
-                            for i in nodes:
-                                if i.id == nID:
-                                    test = 1
-                                    break
-
-                            if test == 1: 
-                                counter += 1
-                            else:
-                                # Add the nodes
-                                nodes.append(Node(nID, nodePort))
-
-                                # Reset the counter if one is found
-                                counter = 0 
-                    break
-                except AttributeError:
-                    print("Error:")
-                   
-    return nodes
-
 
 if __name__== "__main__":
     # Default from bootstrap
     tcp_port = 10732
 
     connections = []
-    
-
-    users = []
-    privateUsers = []
+    net = Network()
 
     # Entry point
-    users.append(Node('80da3d512c17287e6ec3667d0248ced0e9cf1124a46d49fd5447efd20d7ee440', tcp_port))
-
+    net.nodes.append(Node('80da3d512c17287e6ec3667d0248ced0e9cf1124a46d49fd5447efd20d7ee440', tcp_port))
+   
     # Keep track of users we've been to
     contacted = []
 
-    fil = open("peers.txt", 'w')
-
     # For loop
-    for user in users:
+    for user in net.nodes:
         print(user.id)
         if user.id not in contacted:
-            print(user.id)
             soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
             try:
                 soc.connect((TCP_IP, int(user.port)))
             except ConnectionRefusedError:
-                print("Host: (Private)", user.id)
-                if user.id not in privateUsers:
-                    fil.write("Host: (Private) " + user.id + " " + str(tcp_port)  +"\n")
-                    privateUsers.append(user.id)
+                net.addUser(user)
                 continue
             connections.append(soc)
 
+            # try to catch any errors and close connection
             try:
-                peers = findPeers(user.id, soc)
-                fil.write("Host: " + user.id + "\n")
-                print("Host: ", user.id)
-                print('peers: ')
+                peers = net.findPeers(user.id, soc)                  
 
-                for i in peers:
-                    
-                    print(i.id, ' ',end='')
-                    print(i.port)
-                
                 for peer in peers:
-                    if peer not in users:
-                        fil.write("    " + str(peer.id) + " " + str(peer.port) + "\n")
-                        users.append(peer)
+                    net.addUser(peer)
+                    
                 contacted.append(user.id)
-                print("\n\n")
             except KeyboardInterrupt:
                 print("\nClosing")
             except ConnectionResetError as error:
@@ -137,7 +134,9 @@ if __name__== "__main__":
                 #print(re.search('b\'*\n', error))
             except NameError:
                 print("Name Error")
-            #soc.close()
+
+    net.writeNetwork("peers.txt")
+
     # For Loop closing
     for con in connections:
         con.close()
